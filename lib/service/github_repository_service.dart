c2629a45eb213a9e1d1d978cd8afb02767037777
yato <yato28zzzz@gmail.com>
2024-11-01 09:57:29 +0900

    add: Repository検索APIの実装

---
+++ lib/service/github_repository_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:github_repository_search/model/repository.dart';
+
+class GithubRepositoryService {
+  final String _baseUrl = 'https://api.github.com/search/repositories';
+
+  Future<List<Repository>> searchRepository(String searchTerm) async {
+    final url = Uri.parse('$_baseUrl?q=$searchTerm+in:name&sort=stars');
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        final List<dynamic> items = data['items'];
+
+        final repositorys = items.map((e) => Repository.fromJson(e)).toList();
+        return repositorys;
+      } else {
+        throw Exception('Failed to load repositories: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+      return [];
+    }
+  }
+}
